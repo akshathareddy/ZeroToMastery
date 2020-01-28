2d39cfc14994f69f97a0656b7241b4b0c3d38eb1
akshathareddy <akshithareddymandadi@gmail.com>
2020-01-28 22:01:53 +0530

    removed the unnecessary files and Created a cardList for displaying images

--- myreactapp/src/index.js
+++ myreactapp/src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import CardList from './CardList';
+import { robots } from './robots';
+
+import 'tachyons';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(
+	<React-Fragment>
+		<div className="tc code">
+			<h1 className="blue">Robot Friends</h1>
+			<p>Search <input type="text"/> </p>
+		</div>
+		<CardList
+			robots={robots}/>
+	</React-Fragment>
+	, document.getElementById('root'));
 serviceWorker.unregister();
